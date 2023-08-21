0384da3cd999e1843e5249bf51f53e47375ffdc7
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-08-21 19:00:33 -0300

    Preparando app p/ deploy - Deploy do app no Render

--- src/database.ts
+++ src/database.ts
@@ -2,15 +2,20 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
   },
 } as Knex.Config
+
+if (env.DATABASE_CLIENT === 'sqlite') {
+  config.connection = {
+    filename: env.DATABASE_URL,
+  }
+} else {
+  config.connection = env.DATABASE_URL
+}
 
 export const knex = setupKnex(config)
